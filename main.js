1cfbf3407adbcc923cac7f671251b486e060c598
qngxl <hagots@gmail.com>
2024-10-27 13:42:28 +0100

    added container that gets created when button gets pressed

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
 
 function addPasswordButtonOnClick() {
-   alert("saved new password");
+    if(!document.querySelector(".container.create-password")) {
+    const createPasswordContainer = document.createElement("div");
+    createPasswordContainer.classList.add("container", "create-password");
+    createPasswordContainer.textContent = "created new passord"
+    document.body.appendChild(createPasswordContainer);
+    }
 }
 
-document.getElementById("add-password-button").addEventListener("click", addPasswordButtonOnClick)
+document.getElementById("add-password-button").addEventListener("click", addPasswordButtonOnClick);
 
