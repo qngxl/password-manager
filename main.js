7016b19299405e31d1daa72d036fc93866865042
qngxl <hagots@gmail.com>
2024-10-29 14:23:10 +0100

    created 1 instance of input-container-wrapper inside of passwordcontainer with h1 and input in it, /fix: default outline border when stop hovering over input even tho its set to none

--- main.js
+++ main.js
@@ -1,22 +1,36 @@
 
 function createNewPasswordContainerOnClick() {
-    if(!document.querySelector(".container.new-password")) {
+    if(!document.querySelector(".container.new-password-container")) {
 
     const newPasswordContainer = document.createElement("div");
 
-    newPasswordContainer.classList.add("container", "new-password");
-    newPasswordContainer.textContent = "created new passord";
+    newPasswordContainer.classList.add("container", "new-password-container");
+
+    const inputContainerWrapper = document.createElement("div");
+
+    inputContainerWrapper.classList.add("input-container-wrapper");
+
+    const inputHeadline = document.createElement("h1");
+
+    inputHeadline.classList.add("input-headline");
+    inputHeadline.textContent = "test-headline";
+
+    const inputWrapper = document.createElement("input");
+    inputWrapper.classList.add("input-wrapper");
+
 
     const saveButton = document.createElement("button");
 
     saveButton.classList.add("rectangular-button");
-    saveButton
     saveButton.textContent = "Save";
     saveButton.addEventListener("click", () => {
         newPasswordContainer.remove();
     });
 
+    inputContainerWrapper.appendChild(inputHeadline);
+    inputContainerWrapper.appendChild(inputWrapper);
     newPasswordContainer.appendChild(saveButton);
+    newPasswordContainer.appendChild(inputContainerWrapper);
     document.body.appendChild(newPasswordContainer);
     }
 }
