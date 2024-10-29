f420ecd04b25a879112d0fb6d341b56a5bf60756
qngxl <hagots@gmail.com>
2024-10-29 15:14:42 +0100

    created 3 instances of input-container-wrapper and sorted them under each other, gave each one appropriate headlines

--- main.js
+++ main.js
@@ -1,36 +1,40 @@
 
 function createNewPasswordContainerOnClick() {
+
     if(!document.querySelector(".container.new-password-container")) {
 
     const newPasswordContainer = document.createElement("div");
-
     newPasswordContainer.classList.add("container", "new-password-container");
 
-    const inputContainerWrapper = document.createElement("div");
+    const inputLabels = ["Username", "Email", "Password"];
 
+    inputLabels.forEach(label => {   
+
+    const inputContainerWrapper = document.createElement("div");
     inputContainerWrapper.classList.add("input-container-wrapper");
 
     const inputHeadline = document.createElement("h1");
-
     inputHeadline.classList.add("input-headline");
-    inputHeadline.textContent = "test-headline";
+    inputHeadline.textContent = label;  
 
     const inputWrapper = document.createElement("input");
     inputWrapper.classList.add("input-wrapper");
 
+    inputContainerWrapper.appendChild(inputHeadline);
+    inputContainerWrapper.appendChild(inputWrapper);
+
+    newPasswordContainer.appendChild(inputContainerWrapper);
+    });
 
     const saveButton = document.createElement("button");
-
     saveButton.classList.add("rectangular-button");
     saveButton.textContent = "Save";
     saveButton.addEventListener("click", () => {
         newPasswordContainer.remove();
     });
 
-    inputContainerWrapper.appendChild(inputHeadline);
-    inputContainerWrapper.appendChild(inputWrapper);
     newPasswordContainer.appendChild(saveButton);
-    newPasswordContainer.appendChild(inputContainerWrapper);
+
     document.body.appendChild(newPasswordContainer);
     }
 }
